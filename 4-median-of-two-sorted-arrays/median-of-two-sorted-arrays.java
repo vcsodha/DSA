11ba5ac33e767f50e00e061b61a5e5192080a4c4
vcsodha <sodhavidisha@gmail.com>
2026-01-13 21:32:57 -0600

    Time: 2 ms (45.70%) | Memory: 49.2 MB (8.41%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,33 +1,26 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums1.length > nums2.length) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
-
-        int len1 = nums1.length, len2 = nums2.length;
-        int left = 0, right = len1;
-
-        while (left <= right) {
-            int part1 = (left + right) / 2;
-            int part2 = (len1 + len2 + 1) / 2 - part1;
-
-            int maxLeft1 = (part1 == 0) ? Integer.MIN_VALUE : nums1[part1 - 1];
-            int minRight1 = (part1 == len1) ? Integer.MAX_VALUE : nums1[part1];
-            int maxLeft2 = (part2 == 0) ? Integer.MIN_VALUE : nums2[part2 - 1];
-            int minRight2 = (part2 == len2) ? Integer.MAX_VALUE : nums2[part2];
-
-            if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
-                if ((len1 + len2) % 2 == 0) {
-                    return (Math.max(maxLeft1, maxLeft2) + Math.min(minRight1, minRight2)) / 2.0;
-                } else {
-                    return (double) Math.max(maxLeft1, maxLeft2);
-                }
-            } else if (maxLeft1 > minRight2) {
-                right = part1 - 1;
-            } else {
-                left = part1 + 1;
+        int m=nums1.length;
+        int n=nums2.length;
+        int arr[]=new int[m+n];
+        int i=0;   
+        int j=0; 
+        int k=0;  
+        while(i<nums1.length&&j<nums2.length){
+            if(nums1[i]<nums2[j]){
+                arr[k++]=nums1[i++];
+            }else{
+                arr[k++]=nums2[j++];
             }
         }
-        return 0.0;        
+        while(i<nums1.length){
+            arr[k++]=nums1[i++];
+        }
+        while(j<nums2.length){
+            arr[k++]=nums2[j++];
+        }
+        int total=m+n;
+        if(total%2==1)return arr[total/2];
+        return (arr[total/2]+arr[(total/2)-1])/2.0;
     }
 }
