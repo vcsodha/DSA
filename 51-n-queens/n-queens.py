865d65e484729c50337fc09b54ce7efabf60001c
Vidisha Sodha <sodhavidisha@gmail.com>
2026-07-28 14:03:48 -0400

    Time: 3 ms (99.88%) | Memory: 19.6 MB (68.89%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,32 +1,39 @@
 class Solution:
-    def solveNQueens(self, n: int) -> List[List[str]]:
+    def solveNQueens(self, n: int) -> list[list[str]]:
         res = []
-        board = [['.' for _ in range(n)] for _ in range(n)] #empty space
-        cols=set()
-        next_dia=set() 
-        prev_dia=set()
-
-        def backtrack(r):
-            if r == n: #if all rows are filled, found valid solution
-                copy = ["".join(row) for row in board]
-                res.append(copy)
+        board = [["."] * n for _ in range(n)]
+        
+        # cols, pos_diag, neg_diag are now integers (bitmasks)
+        # Using 0 to represent all unoccupied
+        def backtrack(r, cols, pos_diag, neg_diag):
+            if r == n:
+                res.append(["".join(row) for row in board])
                 return
-            for c in range(n):
-                if c in cols or (r + c) in next_dia or (r - c) in prev_dia:
-                    continue
+            
+            # (1 << n) - 1 creates a bitmask of n ones (e.g., for n=4, 1111)
+            # ~(cols | pos_diag | neg_diag) finds all available spots
+            available_bits = ((1 << n) - 1) & ~(cols | pos_diag | neg_diag)
+            
+            while available_bits:
+                # Isolate the lowest set bit (the chosen position)
+                bit = available_bits & -available_bits
+                # Determine column index from the bit
+                c = (bit).bit_length() - 1
                 
-                cols.add(c)
-                next_dia.add(r + c)
-                prev_dia.add(r - c)
-                board[r][c] = 'Q'
+                # Place queen
+                board[r][c] = "Q"
                 
-                backtrack(r + 1)
+                # Move to next row with updated masks
+                # Shift pos_diag left and neg_diag right to account for row change
+                backtrack(r + 1, 
+                          cols | bit, 
+                          (pos_diag | bit) << 1, 
+                          (neg_diag | bit) >> 1)
                 
-                cols.remove(c)
-                next_dia.remove(r + c)
-                prev_dia.remove(r - c)
-                board[r][c] = '.'
-                
-        backtrack(0)
+                # Backtrack
+                board[r][c] = "."
+                # Turn off the bit in available_bits
+                available_bits ^= bit
+        
+        backtrack(0, 0, 0, 0)
         return res
-
