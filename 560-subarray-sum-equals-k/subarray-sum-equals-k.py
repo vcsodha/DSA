931c26c41d08d5bdda84f767c373dc60e95830b2
Vidisha Sodha <sodhavidisha@gmail.com>
2026-08-02 15:34:31 -0400

    Time: 38 ms (30.72%) | Memory: 22 MB (35.25%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,16 +1,15 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        count = 0
-        curr_sum = 0
-        map_sum = {0: 1}
-        
+        count =0
+        curr_sum=0
+        map_sum={0:1} #storing sum of every subarray
         for num in nums:
-            curr_sum += num
-            
-            target = curr_sum - k
+            curr_sum +=num
+            target = curr_sum-k
             if target in map_sum:
                 count += map_sum[target]
             
-            map_sum[curr_sum] = map_sum.get(curr_sum, 0) + 1
-            
+            map_sum[curr_sum] = map_sum.get(curr_sum, 0) + 1 
+        
+        print(map_sum)
         return count 
