2e81e06ba7bd6b828d0244ef80f0c158ff567961
Vidisha Sodha <sodhavidisha@gmail.com>
2026-08-03 23:11:05 -0400

    Time: 891 ms (11.84%) | Memory: 22.6 MB (80.86%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -1,21 +1,31 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        nodes= []
-        for l in lists:
-            curr=l
-            while curr:
-                nodes.append(curr.val)
-                curr=curr.next
-        
-        nodes.sort()
-        dummy=ListNode(0)
-        curr=dummy
-        for val in nodes:
-            curr.next=ListNode(val)
-            curr = curr.next
-        return dummy.next
+
+  def mergeKLists(self, lists):
+    if not lists:
+      return None
+
+    head = lists[0]
+    for i in range(1, len(lists)):
+      head = self.mergeTwoLists(head, lists[i])
+
+    return head
+
+  def mergeTwoLists(self, l1, l2):
+    dummy = ListNode(0)
+    curr = dummy
+
+    while l1 and l2:
+      if l1.val < l2.val:
+        curr.next = l1
+        l1 = l1.next
+      else:
+        curr.next = l2
+        l2 = l2.next
+      curr = curr.next
+
+    if l1:
+      curr.next = l1
+    if l2:
+      curr.next = l2
+
+    return dummy.next
